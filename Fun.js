022b959bfedb2c3e2092b0eba029847fe5ceab53
Dellydell19git config --global user.name Dellydell19git config --global user.name Dellydell19 <carolyntorain@yahoo.com>
2017-06-18 12:08:05 -0500

    My Internet Web Design Template

---
+++ Fun.js
@@ -0,0 +1,24 @@
+// function tellMeAGoodJoke() {
+//   var jokeStr = "Have you heard about corduroy pillowcases?";
+//   jokeStr = jokeStr + " .... They're making headlines!";
+//   jokeStr += "Thanks, I'm here all week...";   // This code never runs!
+//   return jokeStr;
+//   }
+//
+//   console.log(tellMeAGoodJoke());
+//
+//
+// function sayMyName( )
+// {
+//   console.log("My name is Martin");
+// }
+//
+// sayMyName();
+
+var myFriends = ["James", "Jill", "Jane", "Jack"]
+
+  for(var i = 0; i < myFriends.length; i++){
+
+    console.log( myFriends[i] );
+
+    }
---
+++ make_it_fancy.js
@@ -0,0 +1,11 @@
+var myFriends = ["James", "Jill", "Jane", "Jack"]
+
+function theirNames(){
+  for(var i = 0; i < myFriends.length; i++){
+
+    console.log(myFriends[i]);
+
+    }
+}
+
+theirNames();
